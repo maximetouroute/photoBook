29cc0c4e08cc44f5fd6a8f287910f97fe569f6f1
maxioki <maxime.touroute@gmail.com>
2017-11-13 14:47:25 +0100

    Add injectable content

--- src/content/config.js
+++ src/content/config.js
@@ -31,22 +31,39 @@ const landscapesDescription = (
 export const gallery = [
     {
         name: "LANDSCAPES",
-        photos: [],
+        photos: ['res/landscapes/b-33.jpg',
+        'res/landscapes/b-34.jpg',
+        'res/landscapes/b-35.jpg',
+            'res/landscapes/b-37.jpg',
+            'res/landscapes/b-39.jpg'],
         description: landscapesDescription
     },
     {
         name: "PORTRAITS",
-        photos: [],
+        photos: ['res/portraits/b-9.jpg',
+            'res/portraits/b-12.jpg'],
         description: portraitsDescription
     },
     {
         name: "DAILIES",
-        photos: [],
+        photos: ['res/dailies/b-18.jpg',
+            'res/dailies/b-4.jpg',
+            'res/dailies/b-10.jpg',
+            'res/dailies/b-11.jpg',
+            'res/dailies/b-1.jpg',
+            'res/dailies/b-30.jpg'],
         description: dailiesDescription
     },
     {
         name: "EXPERIMENTS",
-        photos: [],
+        photos: ['res/experiments/b-13.jpg',
+            'res/experiments/b-8.jpg',
+            'res/experiments/b-22.jpg',
+            'res/experiments/b-20.jpg',
+            'res/experiments/b-23.jpg',
+            'res/experiments/b-25.jpg',
+            'res/experiments/b-29.jpg',
+            'res/experiments/b-1.jpg'],
         description: experimentsDescription
     },
     {
@@ -55,4 +72,8 @@ export const gallery = [
     }
 ];
 
-export const slideshow = [];
+export const slideshow = [
+    'res/landscapes/b-34.jpg',
+    'res/landscapes/b-39.jpg',
+    'res/experiments/b-2.jpg'
+];
